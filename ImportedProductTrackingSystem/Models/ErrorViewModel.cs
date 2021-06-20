03801192e320a586df2b11f467d3b373f9cac883
ayhncvdr <73463728+ayhncvdr@users.noreply.github.com>
2021-06-20 13:28:15 +0300

    Add project files.

---
+++ ImportedProductTrackingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImportedProductTrackingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
